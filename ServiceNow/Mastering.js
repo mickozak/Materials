477ab6c02b5aaa31e4637578065df15afca98316
Michał Kozak <kozak_m@outlook.com>
2019-03-29 15:53:45 +0100

    Create Mastering.js

---
+++ ServiceNow/Mastering.js
@@ -0,0 +1,5 @@
+1. Relationship tworzy relacje między polami w tej samej tabeli np.: pokoje na tym samy piętrze w hotelu.
+(function refineQuery(current, parent) {   current.addQuery('floor', parent.floor); })(current, parent); 
+2. RelatedList tworzy relacje między listami;
+3. Views widoki z różnym poziomem szczegółowości. System UI > View Rules;
+4. Moduły przechowywane w sys_app_module - ułatwiają nawigację po aplikacjach. Wstawiają odniesienie po lewej stronie;
